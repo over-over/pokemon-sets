08c67481d52e43e4340ac05ad01d80a61ee754fe
Overdoze <klg.panov@gmail.com>
2019-11-01 05:03:49 +0300

    router fix

---
+++ docs/precache-manifest.2664cda113553dee00346082bd71fbb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5a3a53e3e80ee4775f6f5c1eb49672",
+    "url": "./index.html"
+  },
+  {
+    "revision": "657c4223f6af967bfce7",
+    "url": "./static/css/main.f2a47211.chunk.css"
+  },
+  {
+    "revision": "a7643b87af50b0803670",
+    "url": "./static/js/2.42a581eb.chunk.js"
+  },
+  {
+    "revision": "657c4223f6af967bfce7",
+    "url": "./static/js/main.9f022045.chunk.js"
+  },
+  {
+    "revision": "fc3c0822d6890e0d315a",
+    "url": "./static/js/runtime-main.233ea1ec.js"
+  }
+]);
