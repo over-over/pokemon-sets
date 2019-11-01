1d0b2489358281f1baacacbe8a5e648c0c166dec
Overdoze <klg.panov@gmail.com>
2019-11-01 04:35:44 +0300

    docs

---
+++ docs/precache-manifest.f17333d01dcba12bc54315052c2bc131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c4bff3f75214ac671d1fad42eb489c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4710c69a016fdb01f1b",
+    "url": "/static/css/main.f2a47211.chunk.css"
+  },
+  {
+    "revision": "a7643b87af50b0803670",
+    "url": "/static/js/2.42a581eb.chunk.js"
+  },
+  {
+    "revision": "d4710c69a016fdb01f1b",
+    "url": "/static/js/main.25b7e2f9.chunk.js"
+  },
+  {
+    "revision": "8f218d67b7e8d0368e26",
+    "url": "/static/js/runtime-main.50825498.js"
+  }
+]);
