c1fc90bfb28dff69415908685d59fbc32a9022f6
Overdoze <klg.panov@gmail.com>
2019-11-01 05:03:26 +0300

    router fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f17333d01dcba12bc54315052c2bc131.js"
+  "./precache-manifest.2664cda113553dee00346082bd71fbb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CardsPage } from '../pages';
 import './app.css';
-import ErrorIndicator from '../error-indicator';
 
 const App = () => {
     return (
         <main className="container">
             <h1 className="app-header">PokemonTCQ Sets Browser</h1>
             <Switch>
-                <Route path="/" component={HomePage} exact/>
-                <Route path="/set/:id" component={CardsPage}/>
+                <Route path={process.env.PUBLIC_URL + "/"} component={HomePage} exact/>
+                <Route path={process.env.PUBLIC_URL + "/set/:id"} component={CardsPage}/>
             </Switch>
         </main>
     )
